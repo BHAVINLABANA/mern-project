3cc457cca76ce4526d48e81c419b5845265f9823
BHAVINLABANA <bhvainlabana78@gmail.com>
2026-07-24 23:50:25 +0530

    Initial MERN project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">
+        🚀 MERN Project Started Successfully
+      </h1>
+
+      <button className="btn btn-success mt-3">
+        Bootstrap Working
+      </button>
+    </div>
+  );
+}
+
+export default App;
