d9b723432f08f96d75b5ae3ac961945fd0b0418b
BHAVINLABANA <bhvainlabana78@gmail.com>
2026-09-17 18:22:07 +0530

    Prepare project for deployment

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,8 +1,4 @@
 const cloudinary = require("cloudinary").v2;
-console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API Key:", process.env.CLOUDINARY_API_KEY);
-console.log("API Secret:", process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Missing");
-
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
